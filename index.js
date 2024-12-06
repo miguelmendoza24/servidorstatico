68904188c16a4307603d97a39908ca2e74c562b0
miguel <miguelevamendo@gmail.com>
2024-12-06 12:31:45 -0600

    Agregue endpoint /get-contacts para obtener contactos desde MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const mongoDB = require("/MongoDB");
-
+const mongoDB = require("./mongoDB");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -9,13 +8,31 @@ const PORT = process.env.PORT;
 app.use(express.static("public"));
 
 
-app.get("/prueba", (req, res) => {
-  res.send("Hola el servidor esta funcionando.");
+app.use(express.json());
+app.post("/add-contact", async (req, res) => {
+  
+  const { name, phone } = req.body
+
+  const contact = {name, phone}
+  try {
+    await mongoDB.addContactToCollection(contact)
+      res
+        .status(201)
+        .send({ message: "Contacto agregado con éxito"});
+  } catch (error) {
+    res.status(500).send({ message: "Error al agregar el contacto"});
+  }
 });
 
-app.post("/prueba", (req, res) => {
-  res.send("mensaje recibido: ");
+app.get("/get-contacts", async (req, res) => {
+  try {
+    const contacts = await mongoDB.getContacts();
+    res.status(200).send(contacts);
+  } catch(error) {
+    res.status(500).send({message: "Error al obtener los contactos"})
+  }
 });
+
 
 app.put("/prueba", (req, res) => {
   res.send("mensaje actualizado: ");
@@ -25,8 +42,8 @@ app.delete("/prueba", (req, res) => {
   res.send("mensaje eliminado");
 });
 
-app.listen(PORT,  async () => {
+app.listen(PORT, async () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
   await mongoDB.connect();
-   console.log("Conexión exitosa a MongoDB");
+  console.log("Conexión exitosa a MongoDB");
 });
