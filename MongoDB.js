974212505ad3bbd34d2fcd902dc8c1f513b95322
miguel <miguelevamendo@gmail.com>
2024-12-06 12:21:37 -0600

    agregue al modulo mondoDB operecion read

--- MongoDB.js
+++ MongoDB.js
@@ -4,24 +4,53 @@ const MongoClient = require("mongodb").MongoClient;
 function MongoDB() {
   this.db = null;
   this.client = null;
-  this.coleccion = null;
+  this.collection = null;
+  this.generateContactID = function () {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let contactID = "";
+
+    for (let i = 0; i < 12; i++) {
+      contactID += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return contactID;
+  };
 }
 
+//conexion a mongoDB
 MongoDB.prototype.connect = async function () {
   try {
-    const mongoUri = process.env.MONGO_URI; 
+    const mongoUri = process.env.MONGO_URI;
     const dbName = "aprendiendo";
-
     this.client = await MongoClient.connect(mongoUri);
-
-    console.log("Conexión exitosa a MongoDB");
-
     this.db = this.client.db(dbName);
-    this.coleccion = this.db.collection("miColeccion");
-  } catch (error) {
-    console.error("Error al conectar con MongoDB:", error);
+    this.collection = this.db.collection("collectionContacts");
+  } catch (err) {
+    console.error("Error al conectar con MongoDB:", err);
   }
 };
+
+//create
+MongoDB.prototype.addContactToCollection = async function (contact) {
+  try {
+    contact.contactID = this.generateContactID();
+    await this.collection.insertOne(contact);
+    console.log("Contacto agregado con éxito:");
+  } catch (err) {
+    console.error("Error al agregar el contacto:", err);
+  }
+};
+
+//read
+MongoDB.prototype.getContacts = async function () {
+  try {
+    const contacs = await this.collection.find({}).toArray();
+    return contacs
+  } catch (err) {
+    console.error("Error al obtener los contactos:", err);
+  }
+};
+
+//update
 
 module.exports = new MongoDB();
 
