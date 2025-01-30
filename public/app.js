24e722327609165bd2c7332a1fd61f39a43c795d
miguel <miguelevamendo@gmail.com>
2025-01-30 13:42:19 -0600

    app lista para subirse a produccion

--- public/app.js
+++ public/app.js
@@ -1,6 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 
 let contacts = [];
 const book = document.getElementById("book");
@@ -13,6 +10,8 @@ async function renderList() {
   try {
     contactList.innerHTML = "";
     const response = await fetch("http://localhost:3000/get-contacts");
+    console.log(response);
+    
     if (!response.ok) {
       throw new Error(`Error al obtener contactos: ${response.statusText}`);
     }
